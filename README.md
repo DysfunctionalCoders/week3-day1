b7cfb5747df756a86e3e86ad0d6727f4aa745819
Daniel Kim <daniel.seungchul.kim@gmail.com>
2022-03-28 16:48:12 -0700

    week 3 day 1

---
+++ README.md
@@ -0,0 +1,21 @@
+# Vets In Tech Web Development Course
+
+## Week 3 Day 1
+
+### Review [30 minutes]
+- Selectors
+- Flex
+- Page Layout
+- 
+### Learning Objectives
+
+- Data Types Part I [30 minutes]
+  - Assignments, Operators, & Comparisons
+- Data Types Part II [30 minutes]
+  - Arrays
+  - Objects
+
+### Resources
+- [MDN Data Types and Data Structures](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures)
+- [W3 Schools Assignments](https://www.w3schools.com/js/js_assignment.asp)
+  
---
+++ exercise.js
@@ -0,0 +1,25 @@
+// Exercise
+
+// You should have your browser open on exercise.html and refresh to verify after every step.
+// 1.  uncomment below to see javascript immediately in action!
+// window.alert("Hello class!");
+
+// 2. fill in a string value of changeMe inside the method and update the value to VetsInTech!
+document.getElementById(IDSELECTORHERE).innerText = "UPDATE THIS VALUE";
+
+// dont forget to refresh to verify!
+
+// 3. create a variable changed myName assigned to a string value of your name.
+// assign the below element to your variable!
+
+document.getElementById("nameMe").innerText = "";
+
+// did you remember to remove the quotations?
+
+// 4. this one is tricky... take the numeric value of the increaseMe span element
+// increment it and re-assign it back to its innerText.
+
+// we'll start you off by assigning a variable to the innerText.
+// if you console log this variable, you should see the number 2 (hopefully)
+
+var numericValue = document.getElementById("increaseMe").innerText;
